57495dcfaa1af091c91a4ece29c21b47ab9c4594
macmeals <loseworld9731@gmail.com>
2022-02-11 00:33:37 +0900

    取り消し線を記載

--- src/component/TodoApp.jsx
+++ src/component/TodoApp.jsx
@@ -7,6 +7,9 @@ export const TodoApp = () => {
   // 初期値incompTodosに空配列をセット、状態をsetIncompTodosに格納する
   const [IncompTodos, setIncompTodos] = useState([])
 
+  // 初期値compTodosにfalseをセット、状態をsetcompTodosに格納する
+  const [compTodos, setCompTodos] = useState(false)
+
   // テキストボックスで入力した値を保存する
   const changeValue = (e) => setTodoTask(e.target.value)
 
@@ -15,7 +18,19 @@ export const TodoApp = () => {
     if (todoTask === "") return
     const newTodos = [...IncompTodos, todoTask] // 入力されたテキスト（todoTask)を格納し、それらの配列をnewTodosに格納
     setIncompTodos(newTodos) // setIncompTodosにnewTodosの状態を登録
-    setTodoTask("") // setTodoTaskにからの状態を登録
+    setTodoTask("") // setTodoTaskに空の状態を登録
+  }
+
+  // todoリストを削除する関数onDeleteTodoを定義
+  const onDeleteTodo = (index) => {
+    const deleteTodos = [...IncompTodos] // 削除する対象のデータ配列を関数deleteTodoに格納
+    deleteTodos.splice(index, 1) // index番号から１番目の要素を削除
+    setIncompTodos(deleteTodos)
+  }
+
+  // todoリストを完了すれば取り消し線を引く関数completeTodosを定義
+  const completeTodos = () => {
+    setCompTodos(!compTodos) // Trueの状態を設定
   }
 
   return (
@@ -26,12 +41,12 @@ export const TodoApp = () => {
       <button onClick={onAddTodo}>登録</button>
       <h2>TodoList</h2>
       <ul>
-        {IncompTodos.map((todo) => {
+        {IncompTodos.map((todo, index) => {
           return (
             <li key={todo}>
-              <p>{todo}</p>
-              <button>完了</button>
-              <button>削除</button>
+              <p className={compTodos ? "" : "yokosen"}>{todo}</p>
+              <button onClick={completeTodos}>完了</button>
+              <button onClick={() => onDeleteTodo(index)}>削除</button>
             </li>
           )
         })}
@@ -39,3 +54,4 @@ export const TodoApp = () => {
     </div>
   )
 }
+// key={todo}
